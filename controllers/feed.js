22041e348ef2db1268aef5f982931755ef349eb0
rafid-ahmed <rafidrdx@gmail.com>
2024-11-19 16:15:42 +0100

    section 25 part 9

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const validationResult = require('express-validator').validationResult;
 
 const Post = require('../models/post');
@@ -31,7 +34,7 @@ exports.createPost = (req, res, next) => {
         error.statusCode = 422;
         throw error;
     }
-    
+
     const title = req.body.title;
     const imageUrl = req.file.path;
     const content = req.body.content;
@@ -79,4 +82,56 @@ exports.getPost = (req, res, next) => {
         }
         next(err);
     });
+}
+
+exports.editPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation Failed!');
+        error.statusCode = 422;
+        throw error;
+    }
+    const postId = req.params.postId;
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+    if (req.file) {
+        imageUrl = req.file.path;
+    } 
+    if (!imageUrl) {
+        const error = new Error('No file uploaded!');
+        error.statusCode = 422;
+        throw error;
+    }
+    Post.findById(postId)
+    .then(post => {
+        if (!post) {
+            const error = new Error('Could not find post');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        if (imageUrl !== post.imageUrl) {
+            clearImage(post.imageUrl);
+        }
+        
+        post.title = title;
+        post.imageUrl = imageUrl;
+        post.content = content;
+        return post.save();
+    })
+    .then(result => {
+        res.status(200).json({message: 'Post Updated', post: result});
+    })
+    .catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+}
+
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
 }
