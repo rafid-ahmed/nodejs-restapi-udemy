82a8a7336d3707360cf9f7f125ece001f6302309
rafid-ahmed <rafidrdx@gmail.com>
2024-11-19 10:09:04 +0100

    section 25 part 7

--- controllers/feed.js
+++ controllers/feed.js
@@ -6,6 +6,7 @@ exports.getPosts = (req, res, next) => {
     Post.find()
     .then(posts => {
         res.status(200).json({
+            message: 'Fetched posts successfully!',
             posts: posts
         });
     })
@@ -49,3 +50,25 @@ exports.createPost = (req, res, next) => {
         next(err);
     });
 };
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+    .then(post => {
+        if (!post) {
+            const error = new Error('Could not find post');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({
+            message: 'Post fetched!', 
+            post: post
+        });
+    })
+    .catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+}
