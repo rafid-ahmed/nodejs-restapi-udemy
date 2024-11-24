7338b7edf2f0dbc500033470a8b49b7c96f6bf7d
rafid-ahmed <rafidrdx@gmail.com>
2024-11-24 01:41:42 +0100

    section 25 part 18

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const validationResult = require('express-validator').validationResult;
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -48,20 +49,31 @@ exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const imageUrl = req.file.path;
     const content = req.body.content;
+    let creator;
     const post = new Post({
         title: title, 
         imageUrl: imageUrl,
         content: content,
-        creator: {
-            name: 'Kratos'
-        },
+        creator: req.userId,
     });
 
     post.save()
     .then(result => {
+        return User.findById(req.userId);
+    })
+    .then(user => {
+        creator = user;
+        user.posts.push(post);
+        return user.save();
+    })
+    .then(result => {
         res.status(201).json({
             message: 'Post created successfully!',
-            post: result
+            post: post,
+            creator: {
+                _id: creator._id,
+                name: creator.name
+            }
         });
     })
     .catch(err => {
