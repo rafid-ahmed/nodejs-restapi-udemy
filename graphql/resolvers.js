fe493c5c3a7a6976860e8b78244cd63535267480
rafid-ahmed <rafidrdx@gmail.com>
2024-11-30 13:28:41 +0100

    section 28 part 4

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,23 @@
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const User = require('../models/user');
 
 module.exports = {
     createUser: async function ( {userInput}, req) {
+        const errors = [];
+        if (!validator.isEmail(userInput.email))
+        {
+            errors.push(new Error('Email is invalid'));
+        }
+        if (!validator.isLength(userInput.email, {min: 5}))
+        {
+            errors.push(new Error('Password too short'));
+        }
+        if (errors.length > 0) {
+            const error = new Error('invalid input');
+            throw error;
+        }
         const existingUser = await User.findOne({email: userInput.email});
         if (existingUser) {
             const error = new Error('User already exists!');
