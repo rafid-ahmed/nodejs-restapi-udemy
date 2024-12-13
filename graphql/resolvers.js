e63eadbe6dfaaff29a069e7a8079c08e7d2584d7
rafid-ahmed <rafidrdx@gmail.com>
2024-12-14 01:33:28 +0600

    section 28 part 17

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -220,6 +220,42 @@ module.exports = {
         user.posts.pop(id);
         await user.save();
         return true;
+    },
+    user: async function(args, req) {
+        if (!req.isAuth) {
+            const error = new Error('Not authenticated');
+            error.code = 401;
+            throw error;
+        }
+        const user = await User.findById(req.userId);
+        return { ...user._doc, _id: user._id.toString() };
+    },
+    updateStatus: async function({ status }, req) {
+        if (!req.isAuth) {
+            const error = new Error('Not authenticated');
+            error.code = 401;
+            throw error;
+        }
+        if (!validator.isEmpty(status) && !validator.isLength(status, {min: 10})) {
+            {
+                errors.push({ message: 'Status too short' });
+            }
+            if (errors.length > 0) {
+                const error = new Error('Invalid input');
+                error.data = errors;
+                error.code = 422;
+                throw error;
+            }
+        }
+        const user = await User.findById(req.userId);
+        if (!user) {
+            const error = new Error('User not found.');
+            error.code = 401;
+            throw error;
+        }
+        user.status = status;
+        await user.save();
+        return {...user._doc, _id: user._id.toString()};
     }
 };
 
